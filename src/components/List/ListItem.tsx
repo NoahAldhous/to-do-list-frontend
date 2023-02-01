cb42d9bedac84ef1bde6ae3a6a9c8bc9477e96dc
NoahAldhous <naldhous@hotmail.co.uk>
2023-02-01 16:24:57 +0000

    color of checkbox accent changed

--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -7,7 +7,7 @@ const ListItem: React.FC<LiProps> = (item: LiProps) =>  {
     const {action, complete} = item
     return <div className='text-2xl w-80 bg-slate-600 flex justify-end mb-4 rounded-xl'> 
         <p className='mr-4'>{ action }</p>
-        <input  className='mr-4' type='checkbox' checked= {complete ? true : false}/>
+        <input  className='mr-4 accent-orange-500' type='checkbox' checked= {complete ? true : false}/>
     </div>
 }
 
