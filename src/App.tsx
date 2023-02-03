0fb1edd27bb9c1aecb60393b215ea7daedb063fd
NoahAldhous <naldhous@hotmail.co.uk>
2023-02-03 13:03:03 +0000

    added error messge to display if connection to backend is refused

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import {useState} from 'react';
 import './App.css';
 import List from './components/List/List'
 
 function App() {
+
+  const [isError, setIsError] = useState<boolean>(false)
   return (
     <div className="bg-slate-100 w-screen h-screen flex items-center justify-center">
-      <section className="bg-slate-400 w-1/4 min-w-fit h-5/6 flex items-center justify-around flex-col rounded-3xl shadow-2xl">
-        <h1 className="text-4xl text-slate-800 w-full font-bold bg-slate-300"> To-do-List</h1>
-        <List/>
-      </section>
+      {
+        isError 
+        ? <section>
+            <p>OOPS! We can't connect to the server right now. Please try again later.</p>
+          </section>
+        : <section className="bg-slate-400 w-1/4 min-w-fit h-5/6 flex items-center justify-around flex-col rounded-3xl shadow-2xl">
+            <h1 className="flex justify-center text-4xl text-slate-800 w-full font-bold bg-slate-300"> To-Do-List</h1>
+            <List setIsError = {setIsError}/>
+          </section>
+      } 
     </div>
   );
 }
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,24 +1,41 @@
 import ListItem from './ListItem';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-export default function List(){
+export default function List({ setIsError} : {setIsError:React.Dispatch<React.SetStateAction<boolean>>}){
+
 
     const [list, setList] = useState<{}[]|null>(null);
 
     async function fetchList(){
-        const response = await fetch('http://localhost:3001/');
-        const data:{}[] = await response.json();
-        setList(data)
+
+        const url = 'http://localhost:3001/';
+
+        fetch(url)
+        .then( async (response)  => {
+            if (response.ok) {
+                const data:{}[] = await response.json();
+                setList(data)
+                return true;
+            }
+            return Promise.reject(response);
+        })
+        .catch((response => {
+            console.log(response.status, response.statusText);
+            setIsError(true);
+            response.json().then((json: any) => {
+                console.log(json);
+              })
+        }))
     };
 
     fetchList();
 
-    return<section className = ' overflow-y-scroll h-5/6 w-full pl-5 pr-5 min-w-fit flex flex-col items-center'>
+    return<section className = ' overflow-y-scroll h-5/6 w-full pl-5 pr-5 min-w-fit flex flex-col justify-center items-center'>
         {list
             ? list.map( item  => {
                 return <ListItem action={''} complete={false} {...item}/>
             } )
-            : <p>Loading...</p>
+            : <p className='text-2xl'>Loading...</p>
         }
     </section>
 }
