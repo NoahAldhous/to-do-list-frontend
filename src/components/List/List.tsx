0a44cc82a9e75f6de98079ddafd4737ec6ca2365
NoahAldhous <naldhous@hotmail.co.uk>
2023-02-13 22:46:55 +0000

    changed typing of data received from get request to match shape of data in database. now receives data from server correctly, but there is a bug where it continues to send the request even after data is received- could be the connection needs ending on the server side?

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -3,17 +3,19 @@ import React, { useState } from 'react';
 
 export default function List({ setIsError} : {setIsError:React.Dispatch<React.SetStateAction<boolean>>}){
 
-    const [list, setList] = useState<{itemNumber: number, action: string, complete:boolean}[]|null>(null);
+    const [list, setList] = useState<{_id: string, action: string, complete:boolean}[]|null>(null);
 
     async function fetchList(){
 
         const url = 'http://localhost:3001/';
-
-        fetch(url)
+        console.log('sending request to localhost...')
+        await fetch(url)
         .then( async (response)  => {
             if (response.ok) {
-                const data:{user: string, list:{itemNumber: number, action: string, complete:boolean}[]}[] = await response.json();
-                setList(data[0].list)
+                console.log(response)
+                const data:{_id: string, action: string, complete:boolean}[] = await response.json();
+                console.log(data)
+                setList(data)
                 return true;
             }
             return Promise.reject(response);
@@ -32,7 +34,7 @@ export default function List({ setIsError} : {setIsError:React.Dispatch<React.Se
     return<section className = ' overflow-y-scroll h-5/6 w-full pl-5 pr-5 min-w-fit flex flex-col justify-center items-center'>
         {list
             ? list.map( item  => {
-                return <ListItem key = {item.itemNumber} item = {item}/>
+                return <ListItem key = {item._id} item = {item}/>
             } )
             : <p className='text-2xl'>Loading...</p>
         }
--- src/components/List/ListItem.tsx
+++ src/components/List/ListItem.tsx
@@ -1,8 +1,8 @@
 
 
-const ListItem = ({ item }: {item:{ itemNumber: number, action: string; complete: boolean; }}) =>  {
-    const {action, complete, itemNumber} = item
-    return <div key = {itemNumber} className='w-full bg-slate-600 flex justify-end mb-4 rounded-xl pl-4 pr-4 pb-1 pt-1'> 
+const ListItem = ({ item }: {item:{ _id: string, action: string; complete: boolean; }}) =>  {
+    const {action, complete, _id} = item
+    return <div key = {_id} className='w-full bg-slate-600 flex justify-end mb-4 rounded-xl pl-4 pr-4 pb-1 pt-1'> 
         <p className='text-l sm:text-2xl whitespace-nowrap'>{ action }</p>
         <div className='w-2'></div>
         <input  className='accent-orange-500' type='checkbox' checked= {complete ? true : false}/>
