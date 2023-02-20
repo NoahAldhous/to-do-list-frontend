932025863310e56c98df1f259be73c0505983a1f
NoahAldhous <naldhous@hotmail.co.uk>
2023-02-20 18:19:20 +0000

    updated plan.js

--- src/plan.js
+++ src/plan.js
@@ -6,3 +6,7 @@
 //allow to do list items to be edited
 //allow new to do list litems to be added
 //allow to do list items to be deleted
+
+//stretch goals
+//install auth0
+//display individual user's todoList
