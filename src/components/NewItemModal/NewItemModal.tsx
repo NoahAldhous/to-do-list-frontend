9874864b0a909a17557bec6e3479d8d3e07a89c6
NoahAldhous <naldhous@hotmail.co.uk>
2023-02-25 16:52:26 +0000

    added function handleClose which closes the addNewItem modal, and is called when close button is clicked, or user clicks outside of the displayed menu

--- src/components/NewItemModal/NewItemModal.tsx
+++ src/components/NewItemModal/NewItemModal.tsx
@@ -12,7 +12,7 @@ export default function NewItemModal({setIsModal} : {setIsModal:React.Dispatch<R
     }
 
     const handleClose = () => {
-
+        setIsModal(false)
     }
    
     const addItemToDataBase = async() => {
@@ -44,17 +44,17 @@ export default function NewItemModal({setIsModal} : {setIsModal:React.Dispatch<R
     }
 
     return <>
-        <section className= 'z-50 left-0 top-0 fixed w-full h-full bg-none flex flex-col justify-center items-center'>
-            <section className= 'relative flex flex-col justify-center items-center w-1/2 h-1/4 bg-slate-400'>
+        <section className= 'left-0 top-0 fixed w-full h-full flex flex-col justify-center items-center'>
+            <section className= 'z-50 relative flex flex-col justify-center items-center w-1/2 h-1/4 bg-slate-400'>
                 {newItemAdded
                 ? <p>item added!</p>
                 : <p> </p>
                 }
                 <input id='input-box' type='text' onFocus={()=>{setNewItemAdded(false)}} onChange = {updateItemText}></input>
                 <button onClick = {handleClick}>add</button>   
-                <button className='absolute right-0 top-0 mt-1 mr-3' onClick={() => setIsModal(false)}>close X</button>
+                <button className='absolute right-0 top-0 mt-1 mr-3' onClick={handleClose}>close X</button>
             </section>
+        <div onClick={handleClose} className= 'z-40 fixed left-0 top-0 bottom-0 right-0 w-full h-full bg-slate-900 opacity-50'></div>
         </section>
-        <div className= 'z-40 fixed left-0 top-0 bottom-0 right-0 w-full h-full bg-slate-800 opacity-50'></div>
     </>
 };
